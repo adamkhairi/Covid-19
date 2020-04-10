bc66cec7863284b80df07f08e0546897137381d5
adamkhairi <57996941+adamkhairi@users.noreply.github.com>
2020-04-10 03:27:48 +0100

    Quiz Qts and Buttons next and preview #6

--- app/js/quiz.js
+++ app/js/quiz.js
@@ -11,38 +11,37 @@ qts = document.querySelector(".question"); // H3 Question
 qtsh1 = document.querySelector(".quiz__title");
 
 questions = [
-    "Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?",
-    "Quelle est votre température corporelle ?",
-    "Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?",
-    "Avez-vous eu des courbatures inhabituelles au cours des derniers jours ?",
-    "Ces derniers jours, avez-vous un mal de gorge ?",
-    "Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles molles.",
-    "Ces derniers jours, avez-vous une fatigue inhabituelle qui vous a obligé à vous reposer plus de la moitié de la journée ?",
-    "Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?",
-    "Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ?",
-    "Actuellement, comment vous vous sentez ?",
-    "Quel est votre âge ? Ceci, afin de calculer un facteur de risque spécifique.",
-    "Quel est votre poids ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.",
-    "Quelle est votre taille ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.",
-    "Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?",
-    "Êtes-vous diabétique ?",
-    "Avez-vous ou avez-vous eu un cancer ?",
-    "Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?",
-    "Avez-vous une insuffisance rénale chronique dialysée ?",
-    "Avez-vous une maladie chronique du foie ?",
-    "Êtes-vous enceinte ?",
-    "Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?",
-    "Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive).",
+	"Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?",
+	"Quelle est votre température corporelle ?",
+	"Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?",
+	"Avez-vous eu des courbatures inhabituelles au cours des derniers jours ?",
+	"Ces derniers jours, avez-vous un mal de gorge ?",
+	"Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles molles.",
+	"Ces derniers jours, avez-vous une fatigue inhabituelle qui vous a obligé à vous reposer plus de la moitié de la journée ?",
+	"Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?",
+	"Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ?",
+	"Actuellement, comment vous vous sentez ?",
+	"Quel est votre âge ? Ceci, afin de calculer un facteur de risque spécifique.",
+	"Quel est votre poids ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.",
+	"Quelle est votre taille ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.",
+	"Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?",
+	"Êtes-vous diabétique ?",
+	"Avez-vous ou avez-vous eu un cancer ?",
+	"Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?",
+	"Avez-vous une insuffisance rénale chronique dialysée ?",
+	"Avez-vous une maladie chronique du foie ?",
+	"Êtes-vous enceinte ?",
+	"Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?",
+	"Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive).",
 ];
 
 // Button Start Test
 runTest.addEventListener("click", function () {
-    runTestDiv.classList.add("hide");
-    qaDiv.classList.remove("hide");
-    formBtns.classList.remove("hide");
-    qts.innerText = questions[counter];
-
-
+	runTestDiv.classList.add("hide");
+	qaDiv.classList.remove("hide");
+	formBtns.classList.remove("hide");
+	qts.innerText = questions[counter];
+	
 });
 
 // Button of Next Question
@@ -50,20 +49,20 @@ let nxtBtn;
 nxtBtn = document.getElementById('form_back');
 
 function nextQts() {
-    if (qts.innerText === questions[counter]) {
-        counter++;
-
-        document.querySelector(".question").innerText = questions[counter];
-
-    }
+	if (qts.innerText === questions[counter]) {
+		counter++;
+		
+		document.querySelector(".question").innerText = questions[counter];
+		
+	}
 }
 
 // Button of Preview Question
 
 function backQts() {
-
-    if (counter > 0) {
-        counter--;
-        document.querySelector(".question").innerText = questions[counter];
-    }
+	
+	if (counter > 0) {
+		counter--;
+		document.querySelector(".question").innerText = questions[counter];
+	}
 }
