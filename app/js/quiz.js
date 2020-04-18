bc984dfdbe1542e9b3d0ee63f0be04f22f4d5032
adamkhairi <57996941+adamkhairi@users.noreply.github.com>
2020-04-18 02:06:13 +0100

    update #6

--- app/js/quiz.js
+++ app/js/quiz.js
@@ -1,143 +1,118 @@
-let runTest, qsDiv, anDiv, runTestDiv, qaDiv, formBtns, qts, counter, qtsh1;
+let runTest, runTestDiv, formBtns, qtsh1, div, counter, quiz, next, back, start, ans;
 
-counter = 0;
 formBtns = document.querySelector(".form_btns"); // div of buttons
+next = document.querySelector('#form_next');
+back = document.querySelector('#form_back');
+
 runTestDiv = document.querySelector(".quiz__start"); // div of Quiz Start page
-runTest = document.getElementById("startTest"); // Btn Start Quiz in start page
-qaDiv = document.querySelector(".myDiv"); // div of questions and answers
-qsDiv = document.querySelectorAll(".form_question"); // Div of Questions
-anDiv = document.querySelectorAll(".form_answer"); // Div of Answers
-qts = document.querySelector(".question"); // H3 Question
+runTest = document.getElementById("start"); // Btn Start Quiz in start page
 qtsh1 = document.querySelector(".quiz__title");
 
-class quiz {
-	constructor(qst, answers) {
-		this.qst = qst;
-		this.answers = answers;
-	}
-}
-
-let reponse;
-reponse = [
-	new quiz("Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?", ["<label class=\"radio-button\"><input class=\"input\" name=\"form_choice\" type=\"radio\" value=\"oui\" /><span class=\"label-visible\">Oui<span class=\"fake-radiobutton\"></span></span></label>", "<label class=\"radio-button\"><input class='input' name=\"form_choice\" type=\"radio\" value=\"non\" /><span class=\"label-visible\">Non<span class=\"fake-radiobutton\"></span></span></label>"]),
-	new quiz("Quelle est votre température corporelle ?", ["<label class=\"radio-button\"><input class=\"input\" name=\"form_choice\" type=\"radio\" value=\"oui\" /><span class=\"label-visible\">Oui<span class=\"fake-radiobutton\"></span></span></label>", "<label class=\"radio-button\"><input class='input' name=\"form_choice\" type=\"radio\" value=\"non\" /><span class=\"label-visible\">Non<span class=\"fake-radiobutton\"></span></span></label>"]),
-	new quiz("Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?", ["<label class=\"radio-button\"><input class=\"input\" name=\"form_choice\" type=\"radio\" value=\"oui\" /><span class=\"label-visible\">Oui<span class=\"fake-radiobutton\"></span></span></label>", "<label class=\"radio-button\"><input class='input' name=\"form_choice\" type=\"radio\" value=\"non\" /><span class=\"label-visible\">Non<span class=\"fake-radiobutton\"></span></span></label>"]),
-	new quiz("Avez-vous eu des courbatures inhabituelles au cours des derniers jours ?", ["<label class=\"radio-button\"><input class=\"input\" name=\"form_choice\" type=\"radio\" value=\"oui\" /><span class=\"label-visible\">Oui<span class=\"fake-radiobutton\"></span></span></label>", "<label class=\"radio-button\"><input class='input' name=\"form_choice\" type=\"radio\" value=\"non\" /><span class=\"label-visible\">Non<span class=\"fake-radiobutton\"></span></span></label>"]),
-	new quiz("Ces derniers jours, avez-vous un mal de gorge ?", ["<label class=\"radio-button\"><input class=\"input\" name=\"form_choice\" type=\"radio\" value=\"oui\" /><span class=\"label-visible\">Oui<span class=\"fake-radiobutton\"></span></span></label>", "<label class=\"radio-button\"><input class='input' name=\"form_choice\" type=\"radio\" value=\"non\" /><span class=\"label-visible\">Non<span class=\"fake-radiobutton\"></span></span></label>"]),
-	new quiz("Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles molles.", [""]),
-	new quiz("Ces derniers jours, avez-vous une fatigue inhabituelle qui vous a obligé à vous reposer plus de la moitié de la journée ?", [""]),
-	new quiz("Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?", [""]),
-	new quiz("Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ?", [""]),
-	new quiz("Actuellement, comment vous vous sentez ?", [""]),
-	new quiz("Quel est votre âge ? Ceci, afin de calculer un facteur de risque spécifique.", [""]),
-	new quiz("Quel est votre poids ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.", [""]),
-	new quiz("Quelle est votre taille ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.", [""]),
-	new quiz("Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?", [""]),
-	new quiz("Êtes-vous diabétique ?", [""]),
-	new quiz("Avez-vous ou avez-vous eu un cancer ?", [""]),
-	new quiz("Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?", [""]),
-	new quiz("Avez-vous une insuffisance rénale chronique dialysée ?", [""]),
-	new quiz("Avez-vous une maladie chronique du foie ?", [""]),
-	new quiz("Êtes-vous enceinte ?", [""]),
-	new quiz("Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?", [""]),
-	new quiz("Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive).", [""])
-];
+quiz = document.querySelector('.Quiz');
+start = document.getElementById('start');
+counter = 0;
 
 // Button Start Test
 runTest.onclick = () => {
 	runTestDiv.classList.add("hide");
-	qaDiv.classList.remove("hide");
 	formBtns.classList.remove("hide");
-	qts.innerText = reponse[counter].qst;
-	document.querySelector('.form_answer').innerHTML = reponse[counter].answers;
+	quiz.innerHTML = Questions[counter].question;
+	quiz.innerHTML += Questions[counter].rep;
+	ans = document.querySelectorAll('input.form__choice');
+	checkInput();
+	btnControl();
+	next.classList.remove('hide');
 	
-	if (counter === 0) {
-		backBtn.classList.add('hide');
-		
-	}
 };
 // runTest.addEventListener("click", function () {
 //
 // });
 
 // Button of Next Question
-let nxtBtn, backBtn, inpuut, chose, getChose;
-backBtn = document.getElementById('form_back');
-nxtBtn = document.getElementById('form_next');
 
 getChose = [];
 
-let nextQts;
-nextQts = () => {
-	//
-	btnControl();
-	
-	if (qts.innerText === reponse[counter].qst) {
+let btnNext;
+btnNext = () => {
+	if (counter < Questions.length) {
+		
 		counter++;
-		//get the next qst
-		qts.innerText = reponse[counter].qst;
-		
-		//get the next answers choices
-		document.querySelector('.form_answer').innerHTML = reponse[counter].answers;
-		inpuut = document.querySelectorAll('.form_answer .input');
-		
-		//TODO after check
-		//show next Qst
-		//get checked input value
-		
-		for (let j = 0; j < reponse[counter].answers.length; j++) {
-			if (inpuut[j].checked) {
-				getChose.push(inpuut[j].value);
-				alert(getChose);
-			}
-			
-		}
-		alert(getChose);
-		
-		// TODO: FIX THIS
-		//in case didn't chose
+		quiz.innerHTML = Questions[counter].question;
+		quiz.innerHTML += Questions[counter].rep;
+		ans = document.querySelectorAll('input.form__choice');
+		checkInput(ans);
+		btnControl();
 		
 	}
 	
-};
-
-// after first qst
-let btnControl;
-btnControl = () => {
-	// hide next btn in 1qst
-	if (counter === 22) {
-		nxtBtn.classList.add('hide');
-		
-	}
-	//show back btn after 1qst
-	if (counter !== 0) {
-		backBtn.classList.remove('hide');
-		
-	}
 }
-// Button of Back Question
-
-let backQts;
-backQts = () => {
-	//check if not the first qst
-	//and show the last qst
-	//remove from the main table(getChose)
+// Back BTN
+let btnBack;
+btnBack = () => {
 	if (counter > 0) {
 		counter--;
-		// Back QST
-		qts.innerText = reponse[counter].qst;
-		// Back ANS
-		anDiv.html = reponse[counter].answers;
+		quiz.innerHTML = Questions[counter].question;
+		quiz.innerHTML += Questions[counter].rep;
+		ans = document.querySelectorAll('input.form__choice');
 		
-		getChose.pop();
-	}
-	//
-	if (counter === ((reponse.length) - 2)) {
-		alert('HERE');
-		nxtBtn.classList.remove('hide');
+		btnControl();
 		
 	}
 };
+// after first qst
+
+let btnControl;
+btnControl = () => {
+	if (counter === 0) {
+		back.classList.add('hide');
+		
+	} else {
+		back.classList.remove('hide');
+		next.classList.remove('hide');
+		
+	}
+	if (counter === Questions.length - 1) {
+		next.classList.add('hide');
+	}
+};
+
+let checkInput;
+checkInput = (ans) => {
+	ans = document.querySelectorAll('input.form__choice');
+	
+	for (let i = 0; i < ans.length; i++) {
+		ans[i].addEventListener('change', () => {
+			if (ans[i].type === "radio" && ans[i].checked) {
+				
+				let answer;
+				answer = new select();
+				answer.id = counter;
+				answer.answer = ans[i].value;
+				selections.push(answer);
+				// alert(ans[i].value);
+				
+				console.log(selections);
+				
+			}
+			if ((ans[i].type === "text" || ans[i].type === "number") && ans[i].value) {
+				
+				let answertxt;
+				answertxt = new select();
+				// TODO add ...
+				answertxt.id = counter;
+				answertxt.answer = ans[i].value;
+				// inputtxt.push(answertxt);
+				selections.push(answertxt)
+				// alert(ans[i].value);
+				
+				//TODO FIX Error
+				// alert(ans[i].value);
+			}
+		})
+		
+	}
+	
+}
 
 //BTN Animation
 
---
+++ dist/js/content.js
@@ -0,0 +1,109 @@
+let Questions;
+Questions = [
+	//1/////
+	{
+		"question":
+			'<p class="form__question"> Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ? </p>',
+		/// oui . non /////
+		"rep":
+			' <input type="radio" class="form__choice" name="question" value="Oui"><label for="oui">Oui</label> <input type="radio" class="form__choice" name="question" value="Non"><label for="non">Non</label>',
+	},
+	/////2 /////
+	{
+		"question":
+			'<p class="form__question">Quelle est votre température corporelle ?</p>',
+		"rep":
+		///// 1 reponce /////
+			'<input type="number" class=" form__choice choiceTxt" placeholder="34 - 42" min="34" max="42"> <label>°c</label>',
+	},
+	//////3/////
+	{
+		"question":
+			'<p class="form__question"> Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?</p>',
+		/// oui . non /////
+		"rep":
+			' <input type="radio" class="form__choice" name="question" value="Oui"><label for="oui">Oui</label> <input type="radio" class="form__choice" value="Non" name="question"><label for="non">Non</label>',
+	},
+	/////4/////
+	{
+		"question":
+			'<p class="form__question"> Avez-vous eu des courbatures inhabituelles au cours des derniers jours ? </p>',
+		/// oui . non /////
+		"rep":
+			' <input type="radio" class="form__choice" name="question" value="Oui"><label for="oui">Oui</label> <input type="radio" class="form__choice" value="Non" name="question"><label for="non">Non</label>',
+	},
+	//// 5 ./////
+	{
+		"question":
+			'<p class="form__question"> Ces derniers jours, avez-vous un mal de gorge ? </p>',
+		/// oui . non /////
+		"rep":
+			' <input type="radio" class="form__choice" value="Oui" name="question"><label for="oui">Oui</label>  <input type="radio" class="form__choice" value="Non" name="question"><label for="non">Non</label>',
+	},
+	//////6/////
+	{
+		"question":
+			'<p class="form__question"> Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles molles. </p>',
+		/// oui . non /////
+		"rep":
+			' <input type="radio" class="form__choice" value="Oui" name="question"><label for="oui">Oui</label>  <input type="radio" class="form__choice" value="Non" name="question"><label for="non">Non</label>',
+	},
+	/////7//////
+	{
+		"question":
+			'<p class="form__question">Ces derniers jours, avez-vous une fatigue inhabituelle qui vous a obligé à vous reposer plus de la moitié de la journée ? </p>',
+		/// oui . non /////
+		rep:
+			' <input type="radio" class="form__choice" value="Oui" name="question"><label for="oui">Oui</label>  <input type="radio" class="form__choice" value="Non" name="question"><label for="non">Non</label>',
+	},
+	//////8/////
+	{
+		"question":
+			'<p class="form__question">Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ? </p>',
+		/// oui . non /////
+		"rep":
+			' <input type="radio" class="form__choice" value="Oui" name="question"><label for="oui">Oui</label>  <input type="radio" class="form__choice"value="Non" name="question"><label for="non">Non</label>',
+	},
+	//////9/////
+	{
+		"question":
+			'<p class="form__question"> Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ? </p>',
+		/// oui . non /////
+		"rep":
+			' <input type="radio" class="form__choice" value="Oui" name="question"><label for="oui">Oui</label>  <input type="radio" class="form__choice" value="Non" name="question"><label for="non">Non</label>',
+	},
+	///////10////
+	{
+		"question":
+			'<p class="form__question">Actuellement, comment vous vous sentez ? </p>',
+		"rep":
+		//// 4reponce////
+			' <input type="radio" class="form__choice" value="Bien" name="question"><label for="bien">Bien</label><input type="radio" class="form__choice" value="assez-bien" name="question"><label for="assez-bien"> Assez bien</label><input type="radio" class="form__choice" value="fatigue" name="question"><label for="fatigue">Fatigué(e) </label><input type="radio" class="form__choice" value="t-fatigue" name="question"><label for="t-fatigue"> Très fatigué(e)</label>',
+	},
+	
+	// {
+	//   question:
+	//     '<p class="form__question">Actuellement, comment vous vous sentez ? </p>',
+	//   rep:
+	//     //// 4reponce////
+	//     ' <input type="radio" id="form__choice" name="question"><label for="oui">test</label><input type="radio" id="form__choice" name="question"><label for="non">test</label><input type="radio" id="form__choice" name="question"><label for="oui">test</label><input type="radio" id="form__choice" name="question"><label for="non">test</label>',
+	// },
+	// {
+	//   question:
+	//     '<p class="form__question">Quel est votre âge ? Ceci, afin de calculer un facteur de risque spécifique.</p>',
+	//   rep:
+	//     ///// 1 reponce /////
+	//     '<input type="number" name="question2" placeholder="34 - 42" min="34" max="42"> <label>°c</label>',
+	// },
+];
+let selections;
+selections = [];
+let inputtxt;
+inputtxt = [];
+
+class select {
+	constructor(id, answer) {
+		this.id = id;
+		this.answer = answer;
+	}
+}
